ba22366c5f6d77bf0bf6b2c747f37b17d3423722
Grimfeld <48754678+grimfeld@users.noreply.github.com>
2022-07-31 16:11:10 +0200

    Fixed heroku port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 // const Bundler = require("parcel-bundler");
 // const bundler = new Bundler("src/index.html", { logLevel: 2 });
 
-const PORT = process.env.port || 3001
+const PORT = process.env.PORT || 3001
 
 app.use(cors())
 
